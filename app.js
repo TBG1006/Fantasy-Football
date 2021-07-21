76b63dc7a3ca241080dc1d007aefe0e45cc5123c
Tom Garrity <tomgarrity2020@gmail.com>
2021-07-21 19:32:37 -0400

    dropdown

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
+
+d3.selectAll("#positionDropDown").on("change", statChart);
+
 function statchart(adpData) {
-    d3.json(flaskurl).then(data => {
-        var statTable = d3.select('tbody');
-
-
-    })
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("#positionDropDown");
+    // Assign the value of the dropdown menu option to a variable
+    var dataset = dropdownMenu.property("value");
+  
+    // // Initialize x and y arrays
+    // var x = [];
+    // var y = [];
+  
+    if (dataset === 'QB') {
+        d3.json("http://127.0.0.1:5000/api/v1.0/QB").then(data => {
+            var statTable = d3.select('tbody');
+            console.log(data);
+        })
+    }
+  
+        // else if (dataset === 'RB') {
+        //     x = [10, 20, 30, 40, 50];
+        //     y = [1, 10, 100, 1000, 10000];
+        // }
 }
-
-function adpGraph(adpData) {
-    d3.json(flaskurl).then(data => {
+         
+// function adpGraph(adpData) {
+//     d3.json(flaskurl).then(data => {
         
-    })
+//     })
 
-}
+// }
 
 function init(){
     var dropDown = d3.select("#positionDropDown");
@@ -29,7 +47,7 @@ function init(){
 
 function optionChange(newAdpData){
     function statchart(newAdpData);
-    function adpGraph(newAdpData);
+    // function adpGraph(newAdpData);
 }
 
 init();

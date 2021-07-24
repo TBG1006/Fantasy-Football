e80ad2dc41b00ff2053c997f7da4b813b33209c3
Nicole <nclotus11@gmail.com>
2021-07-24 16:16:51 -0400

    commit -m "css updates and bubblechart text"

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+var y0 = [];
+var y1 = [];
+for (var i = 0; i < data.length; i ++) {
+	y0[i] = Math.random();
+	y1[i] = Math.random() + 1;
+}
+
+var trace1 = {
+  y: y0,
+  type: 'box'
+};
+
+var trace2 = {
+  y: y1,
+  type: 'box'
+};
+
+var data = [trace1, trace2];
+
+Plotly.newPlot('myDiv', data);
+
+
+function boxPlot() {
+    d3.json("/api/v1.0/Projected_Data").then( PjPts => {
+        
+    })
+}
+
 
 d3.selectAll("#positionDropDown").on("change", statChart);
 

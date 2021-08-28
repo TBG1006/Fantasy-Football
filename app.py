86845b6deef549a61e82d8ae1a8c094d2effc9d8
Nicole <nclotus11@gmail.com>
2021-08-28 14:20:33 -0400

    update site with ML page, css, flask app, and image folder

--- app.py
+++ app.py
@@ -57,6 +57,10 @@ def about():
 @app.route("/index.html")
 def home(): 
     return render_template("index.html") 
+    
+@app.route("/predictions.html")
+def predictions(): 
+    return render_template("predictions.html")
 
 @app.route("/api/v1.0/Highlights")
 def highlights_data():
